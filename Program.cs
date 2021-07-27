22654519ca2b633b663d9cac6fc25de2b8189074
Ali <malicicek1@gmail.com>
2021-07-28 02:16:36 +0300

    CodilityCyclicRotation

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ArrayRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 3, 8, 9, 7, 6 };
+            ArrayRotation.Rotate(arr, 3);
+        }
+    }
+
+    public static class ArrayRotation
+    {
+        public static int[] Rotate(int[] A, int K)
+        {
+            if (A.Length == K)
+                return A;
+
+
+            int[] temp = new int[A.Length];
+            for (int i = 0; i < A.Length; i++)
+            {
+                temp[i] = A[i];
+            }
+
+            var res = temp;
+            for (int i = 0; i < A.Length; i++)
+            {
+                if ((i + K) < A.Length)
+                {
+                    res[i + K] = A[i];
+                }
+                else if ((i + K) > A.Length)
+                {
+                    res[((i + K) % A.Length)] = A[i];
+                }
+                else if (((i + K) % A.Length) == 0)
+                    res[0] = A[i];
+            }
+            return res;
+        }
+    }
+}
